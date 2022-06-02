565dbb4c8b112eebc65fe9d30dc3d67c246f3d9c
kiwookim <kwedgemusic@gmail.com>
2022-06-02 13:18:05 -0700

    used React Query

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import Chart from './chart';
 import Price from './price';
 import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { fetchCoinInfo, fetchCoinTickers } from '../api';
+import { stringify } from 'querystring';
 
 const Tabs = styled.div`
   display: grid;
@@ -141,26 +144,18 @@ function Coin () {
   const {coinId} = useParams<{coinId:string}>();
   const location = useLocation();
   const name = location.state as RouterState;
-  const [info, setInfo] = useState<IInfoData>();
-  const [priceInfo, setPriceInfo] = useState<IPriceData>();
   const priceMatch = useMatch("/:coinId/price");
   const chartMatch = useMatch("/:coinId/chart");
-  const [loading, setLoading] = useState(true);
-  useEffect(()=>{
-    (async ()=> {
-      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }
-  ,[coinId])
+
+  const {isLoading : infoLoading, data: infoData} = useQuery(['info',coinId], ()=> fetchCoinInfo(coinId ? coinId : ""));
+  const {isLoading: tickerLoading,data: tickerData} = useQuery(['tickers',coinId], ()=> fetchCoinTickers(coinId ? coinId : ""));
+  const loading = infoLoading || tickerLoading;
+
   return (
     <Container>
       <Header>
         <Title>
-          {name?.name ? name.name : loading ? "Loading..." : info?.name}
+          {name?.name ? name.name : loading ? "Loading..." : infoData?.name}
         </Title>
       </Header>
       {loading ? (
@@ -170,26 +165,26 @@ function Coin () {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickerData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickerData?.max_supply}</span>
             </OverviewItem>
           </Overview>
 
--- src/routes/coinsss.tsx
+++ src/routes/coinsss.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import React, { useEffect, useState } from 'react';
 import { ReactDOM } from 'react';
+import { useQuery } from 'react-query';
+import { fetchCoins } from '../api';
 
 
 const Container = styled.div`
@@ -67,14 +69,7 @@ interface CoinInterface {
 
 
 function Coinsss () {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  useEffect(()=>{
-    (async() => {
-      const response = await fetch('https://api.coinpaprika.com/v1/coins');
-      const json = await response.json();
-      setCoins(json.slice(0,100));
-    })();
-  },[]);
+  const {isLoading, data} = useQuery<CoinInterface[]>('allCoins', fetchCoins);
 
 
   return (
@@ -83,7 +78,7 @@ function Coinsss () {
         <Title>Crypto Coin Tracker</Title>
       </Header>
       <CoinList>
-        {coins.map((coin)=>
+        {data?.slice(0,100).map((coin)=>
           <Coin key= {coin.id}>
             <Link to = {`/${coin.id}`} state = {{name: coin.name}}>
               <Image src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt="coin-image" />
