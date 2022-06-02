4aa800aaae364f3c024020e5aaaffd6dcf7e9e27
kiwookim <kwedgemusic@gmail.com>
2022-06-02 11:03:57 -0700

    up to nested routes

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,8 +1,36 @@
 import { useEffect,useState } from 'react';
-import { useParams ,useLocation } from 'react-router-dom';
+import { Outlet, Routes, Route, useParams ,useLocation, useMatch } from 'react-router-dom';
 import styled from 'styled-components';
+import Chart from './chart';
+import Price from './price';
+import { Link } from 'react-router-dom';
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
 
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 const Container = styled.div`
   padding: 0px 15px;
@@ -23,6 +51,30 @@ const Title = styled.h1`
   color : ${props => props.theme.accentColor};
   font-size: 48px;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+  line-height: 1.5;
+`;
+
 
 interface RouterState {
   name: string;
@@ -91,24 +143,68 @@ function Coin () {
   const name = location.state as RouterState;
   const [info, setInfo] = useState<IInfoData>();
   const [priceInfo, setPriceInfo] = useState<IPriceData>();
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+  const [loading, setLoading] = useState(true);
   useEffect(()=>{
     (async ()=> {
       const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-      console.log(infoData);
       const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-      console.log(priceData);
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
   }
-  ,[])
+  ,[coinId])
   return (
     <Container>
       <Header>
         <Title>
-          {name.name}
+          {name?.name ? name.name : loading ? "Loading..." : info?.name}
         </Title>
       </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
+          <Outlet/>
+        </>
+      )}
     </Container>
   );
 }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Coin from './coin';
 import Coinsss from './coinsss';
+import Chart from './chart';
+import Price from './price';
 
 function Router () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/:coinId' element = {<Coin/>}>
+          <Route path="chart" element={<Chart />} />
+          <Route path="price" element={<Price />} />
         </Route>
         <Route path ='/' element={<Coinsss/>}>
         </Route>
       </Routes>
-    
     </BrowserRouter>
   );
 }
